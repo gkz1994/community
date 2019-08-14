89cb83043f1cd7723d8f54761a4bbde882642594
gkz1994 <597643514@qq.com>
2019-08-14 14:04:18 +0800

    init repository

---
+++ src/main/java/com/gkzcode/community/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.gkzcode.community.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
+/**
+ * @ClassName controller
+ * @Description TODO
+ * @Author gkz
+ * @Date 2019/8/12 04:36
+ * @Version 1.0
+ **/
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name",required = false,defaultValue = "world")String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
