82d0d32f9971020eb718c98fcd87af2ee2c4b12e
gkz1994 <597643514@qq.com>
2019-08-17 01:36:24 +0800

    database init

--- src/main/java/com/gkzcode/community/mapper/UserMapper.java
+++ src/main/java/com/gkzcode/community/mapper/UserMapper.java
@@ -7,6 +7,6 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface UserMapper {
 
-    @Insert("insert into user(ACCOUNT_ID,name,token,GMT_CREATE,GMT_MODIFIED)values(${accountId,name,token,gmtCreate,gmtModified})")
+    @Insert("insert into user (ACCOUNT_ID,name,token,GMT_CREATE,GMT_MODIFIED)values(#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 }
